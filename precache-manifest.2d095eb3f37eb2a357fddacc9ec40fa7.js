fb1ab92cd4c8cd34736748a8b5fb009ba3dcd296
lavieennoir <dmitriyKhiznyak@gmail.com>
2019-03-26 00:48:38 +0200

    Updates

---
+++ precache-manifest.2d095eb3f37eb2a357fddacc9ec40fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41d7d8264a454f21780b",
+    "url": "/react-test-app/static/js/runtime~main.cbd17629.js"
+  },
+  {
+    "revision": "66181cbafd82eac78044",
+    "url": "/react-test-app/static/js/main.ba426991.chunk.js"
+  },
+  {
+    "revision": "781168aceb861492112f",
+    "url": "/react-test-app/static/js/2.8f7fbfda.chunk.js"
+  },
+  {
+    "revision": "66181cbafd82eac78044",
+    "url": "/react-test-app/static/css/main.64351bec.chunk.css"
+  },
+  {
+    "revision": "43a8f6f07b047dc6f03a198d0991a5bd",
+    "url": "/react-test-app/index.html"
+  }
+];
